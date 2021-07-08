6546898b39a4ea3d38eca3373fe57d97fa912568
Adrian Li <xiaxi.li.syd@gmail.com>
2021-07-08 11:11:48 +1000

    rewrite useLocalStorage hook

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 
 export default function useLocalStorage() {
-  const [favorites, setFavorites] = useState(() => {
-    if (typeof window !== 'undefined')
-      JSON.parse(window.localStorage.getItem('sw-favorites')) || [];
-    return [];
-  });
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(
+    () =>
+      setFavorites(
+        JSON.parse(window.localStorage.getItem('sw-favorites')) || []
+      ),
+    []
+  );
 
   useEffect(
     () =>
